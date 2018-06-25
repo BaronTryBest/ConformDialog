0f8776b004f7b96495b827e66a86534870c33288
PXiaoPing <2385733147@qq.com>
2018-06-25 09:35:41 +0800

    add second system.out

--- app/src/main/java/com/hz/pxp/conformdialog/MainActivity.java
+++ app/src/main/java/com/hz/pxp/conformdialog/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
         //helloworld
         System.out.println("dsafa");
+
+        //second update
+        System.out.println("2222");
     }
 }
